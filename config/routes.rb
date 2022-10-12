47b6352205dd3e04664f6d75d16eab22546f43e0
Kalamojo <kojo22444488888888@gmail.com>
2022-10-12 14:19:26 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resources :posts
+  root to: "tasks#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
